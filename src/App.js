b36e11473b8abbe664657784209879fb11673df8
father152 <tsimbalenko.vv@gmail.com>
2023-12-17 10:41:40 +0300

    41

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const cash = useSelector(state => state.cash.cash);
+  const customers = useSelector(state => state.customers.customers);
+
+  const addCash = (cash) => {
+    dispatch({ type: "ADD_CASH", payload: cash });
+  };
+  const getCash = (cash) => {
+    dispatch({ type: "GET_CASH", payload: cash });
+  };
+  const addCustomer = (name) => {
+    const customer = {
+      name,
+      id: Date.now()
+    }
+    dispatch({ type: "ADD_CUSTOMER", payload: customer });
+  }
+  const removeCustomer = (customer) => {
+  dispatch({type: "REMOVE_CUSTOMERS", payload: customer.id})
+}
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ fontSize: "25px" }}>Balance: {cash}</div>
+      <div style={{ display: "flex" }}>
+        <button onClick={() => addCash(Number(prompt()))}>Пополнить счет </button>
+        <button onClick={() => getCash(Number(prompt()))}>Снять со счета</button> 
+        <button onClick={() => addCustomer(Number(prompt()))}>Добавить клиента</button>
+        {/* <button onClick={() => removeCustomers(Number(prompt()))}>Удалять клиента</button>  */}
+        
+                  
+      </div>
+
+      {customers.length > 0 ? 
+        <div>
+          {customers.map(customer => <div onClick={() => removeCustomer(customer)}>{customer.name}</div>)
+                      }
+        </div>
+      : 
+        <div style={{ fontSise: "30px" }}>Клиенты отсутствуют!</div>
+      }
     </div>
   );
 }
